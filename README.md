baab2f2f31f58d3c1213f928c929da5c899b7c47
Chris Bragg <cbragg9@yahoo.com>
2020-03-29 17:53:18 -0700

    Local storage saving

--- README.md
+++ README.md
@@ -2,10 +2,11 @@
 Work Day Planner  
 
 # Latest Commit  
--Set currentDay <p> ID to current date  
--Created function to change text area color (through class) depending on past/present/future  
+-Added function to save corresponding text area to local storage on save button click  
 
 # Previous Commits  
+-Set currentDay ID to current date  
+-Created function to change text area color (through class) depending on past/present/future  
 -Added rows for 9AM to 5PM in html  
 -Added time with CSS styling, text area, and button to rows  
 -Uploaded FontAwesome save icon  
--- script.js
+++ script.js
@@ -1,16 +1,26 @@
-// Set currentDay ID to current date
-$("#currentDay").html(moment().format("MMM Do YYYY"));
+$( document ).ready(function() {
 
-var timeOptions = [9, 10, 11, 12, 13, 14, 15, 16, 17];
-var currentTime = moment().format("H");
+    // Set currentDay ID to current date
+    $("#currentDay").html(moment().format("MMM Do YYYY"));
 
-// Change text area color (through class) depending on past/present/future
-for (var i = 0; i < timeOptions.length; i++) {
-    if (currentTime < timeOptions[i]) {
-        $("#" + timeOptions[i] + "-time-row").addClass("future");
-    } else if (currentTime > timeOptions[i]) {
-        $("#" + timeOptions[i] + "-time-row").addClass("past");
-    } else if (currentTime = timeOptions[i]) {
-        $("#" + timeOptions[i] + "-time-row").addClass("present");
+    var timeOptions = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+    var currentTime = moment().format("H");
+
+    // Change text area color (through class) depending on past/present/future
+    for (var i = 0; i < timeOptions.length; i++) {
+        if (currentTime < timeOptions[i]) {
+            $("#" + timeOptions[i] + "-time-row").addClass("future");
+        } else if (currentTime > timeOptions[i]) {
+            $("#" + timeOptions[i] + "-time-row").addClass("past");
+        } else if (currentTime = timeOptions[i]) {
+            $("#" + timeOptions[i] + "-time-row").addClass("present");
+        }
     }
-}
+
+    // Save corresponding text area to local storage on save button click
+    $(".saveBtn").on("click", function(event) {
+        var id = $(this).attr('id');        
+        var textAreaContents = $("#" + id + "-time-row").val();
+        localStorage.setItem(id, textAreaContents);
+    });
+});
